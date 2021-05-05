2114834b5688055996f296801b0161b5b37ef9ff
annviik <48365158+annviik@users.noreply.github.com>
2021-05-06 01:00:36 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Movie from "./components/Movie";
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=59bb077760fa1840166f072ef6b239a7&page=1";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=59bb077760fa1840166f072ef6b239a7&query=";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [ movies, setMovies ] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+     fetch(FEATURED_API)
+     .then((res) => res.json())
+     .then((data) => {
+       setMovies(data.results);
+     });
+
+  }, []);
+  const handleOnSubmit = (e) => {
+      e.preventDefault();
+  if(searchTerm) {
+      fetch(SEARCH_API + searchTerm)
+     .then((res) => res.json())
+     .then((data) => {
+       setMovies(data.results);
+     }); 
+     setSearchTerm(''); }
+     
+  };
+
+  const handleOnchange = (e) => {
+    setSearchTerm(e.target.value);
+
+  }
+return ( 
+   <>
+   
+    <header>
+    <h1>The Movie Dashboard</h1>
+      <form onSubmit ={handleOnSubmit}>
+      <input 
+    className= "search" 
+    type="search" 
+    placeholder="Search For Movies"
+    value={searchTerm}
+    onChange= {handleOnchange}
+    />
+      </form> 
+     </header> 
+     
+       <div className= "movie-container">
+         
+         {movies.length > 0 && 
+           movies.map((movie) => 
+             <Movie key= {movie.id} {...movie} />)}
+          </div> 
+          </>
+);
 }
 
 export default App;
